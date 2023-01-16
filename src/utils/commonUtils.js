c52f352ef26a79a7deed037d14042657553920db
simon <simon.lu@tron.network>
2023-01-16 14:17:35 +0800

    feat: question lists

--- src/utils/commonUtils.js
+++ src/utils/commonUtils.js
@@ -4,7 +4,10 @@ export const checkQuestionId = (questionId, questionCount) => {
     return false;
   }
 
-  if (parseInt(questionId) > parseInt(questionCount) || parseInt(questionId) < 0) {
+  if (
+    parseInt(questionId) > parseInt(questionCount) ||
+    parseInt(questionId) < 0
+  ) {
     console.error(`questionId ${questionId} is not in question list`);
     return false;
   }
@@ -15,12 +18,16 @@ export const checkQuestionId = (questionId, questionCount) => {
 export const triggerConstant = async (contractObj, methodName, ...params) => {
   try {
     if (params.length > 0) {
-      return await contractObj[methodName](...params).call({ _isConstant: true });
+      return await contractObj[methodName](...params).call({
+        _isConstant: true,
+      });
     } else {
       return await contractObj[methodName]().call({ _isConstant: true });
     }
   } catch (err) {
-    console.error(`triggerConstant contractObj=${contractObj}, methodName=${methodName}, params=${params}, err=${err}`);
+    console.error(
+      `triggerConstant contractObj=${contractObj}, methodName=${methodName}, params=${params}, err=${err}`
+    );
     return "";
   }
 };
